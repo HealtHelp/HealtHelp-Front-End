c04b95537a0117ca2979b980a3d80674f00d0bbb
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2019-03-17 05:24:12 +0100

    Access-Control-Allow-Origin server rules added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import axios from 'axios';
 import PrimarySearchAppBar from './modules/header/components/header.component';
 import BottomAppBar from './modules/footer/components/footer.component';
 import ScrollableTabsButtonForce from './modules/tab/components/tab.component';
@@ -10,6 +11,48 @@ import TextMobileStepper from './modules/services/components/services.component'
 import FormDialog from './modules/login/components/login.component';
 import {setHandleChangeTab} from './modules/tab/actions';
 import {setHandleTab} from './modules/header/actions';
+import moment from 'moment';
+
+function testCallApi(){
+    
+  let now = moment().format('YYYY-MM-DD');
+  console.log(`Call APIRest HealtHelp: ${now}`);
+  const url = `http://localhost:8088/api/login/`;
+  console.log(url)
+    
+  const config={
+    headers:{
+      "Access-Control-Allow-Origin":"http://localhost:3000",
+      "Access-Control-Request-Method": "POST, GET, DELETE, PUT"
+    }
+  }
+  
+    const data = {
+      username:"admin",
+      password:"admin"
+    }
+   
+    
+    const axios = require('axios'); 
+    axios.post(url,data,config)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    }); 
+
+    /*fetch(url, {  
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body:{
+        username: 'admin', password: 'admin'
+       }
+    })*/
+}
 
 
 class App extends Component {
@@ -21,8 +64,9 @@ class App extends Component {
      
     }
   }
-
+  
   handleTab = (showNav) =>{
+    testCallApi();
     this.setState({showNav:showNav})
     this.props.setHandleTab(showNav);
   }
@@ -33,9 +77,9 @@ class App extends Component {
   }
 
 
-
   render() {
     return (
+      
       <div className="App">
       <PrimarySearchAppBar handleTab={this.handleTab}></PrimarySearchAppBar>
       {this.state.showNav === true ? <ScrollableTabsButtonForce handleLogin={this.handleLogin} handleChangeTab={this.handleChangeTab}></ScrollableTabsButtonForce> : ''}
