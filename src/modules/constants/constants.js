34163ca0f7fe6981d0a1ae8ce34f040dc3de872c
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2019-06-12 10:23:18 +0200

    fix:merging

--- src/modules/constants/constants.js
+++ src/modules/constants/constants.js
@@ -1,15 +1,9 @@
 
 export const APIKEY = 'AIzaSyDqyFTY_ZXXxEXJxEpXmKQ6jPSs3GKF5qw';
-export const urlLogin = `http://localhost:8088/login/`;
+export const URL_LOGIN = `http://localhost:8088/login/`;
 export const urlActuatorInfo = `http://localhost:8088/api/actuator/info`;
-export const TABSTART = 0;
-export const TABCLINIC = 1;
-export const TABSERVICES = 2;
-export const TABAPPOINTMENT = 3;
-export const TABCONTACT = 4;
-export const TABLOGIN = 5;
 export const CONFIGHEADERS = {
     headers:{
-      "Access-Control-Allow-Origin":"http://localhost:3001"
+      "Access-Control-Allow-Origin":"http://localhost:3000"
     }
   }
--- src/modules/login/actions/login.actions.js
+++ src/modules/login/actions/login.actions.js
@@ -1,18 +1,13 @@
-
 import {SET_HANDLE_LOGIN} from '../types/types';
-
 import axios from 'axios';
-import { CONFIGHEADERS } from '../../constants/constants';
+import { CONFIGHEADERS,URL_LOGIN } from '../../constants/constants';
 
 export const handleLogin = (data) =>  dispatch => {
-    axios.post('http://localhost:8088/api/login', data, CONFIGHEADERS)
+    axios.post(URL_LOGIN, data, CONFIGHEADERS)
         .then((res) => {
-            localStorage.setItem("Email",data.email)
-            localStorage.setItem("Password",data.password)
             return dispatch({
                 type: SET_HANDLE_LOGIN,
-                resp: res.data,
+                resp: res.data
             })
         });
-    
 }
