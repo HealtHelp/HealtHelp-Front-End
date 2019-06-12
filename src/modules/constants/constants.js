3e45ba0d148d686a8a23aa577f1692b91981eece
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2019-06-12 10:13:56 +0200

    fix:change constants URLLOGIN

--- src/modules/constants/constants.js
+++ src/modules/constants/constants.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 export const APIKEY = 'AIzaSyDqyFTY_ZXXxEXJxEpXmKQ6jPSs3GKF5qw';
 export const urlLogin = `http://localhost:8088/login/`;
-=======
-export const urlLogin = `http://localhost:8088/api/login/`;
->>>>>>> fed16bd72e9b6b1765aa20bc27d881a3346843c7
 export const urlActuatorInfo = `http://localhost:8088/api/actuator/info`;
 export const TABSTART = 0;
 export const TABCLINIC = 1;
